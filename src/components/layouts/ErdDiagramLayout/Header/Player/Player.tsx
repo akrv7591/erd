4e464a4ec3688830a82906d6bb18d2835111747e
a7591 <a7591@zeroweb.kr>
2023-12-19 17:44:59 +0900

    remove subscrption to yourself and added bring fron on subscribed

--- src/components/layouts/ErdDiagramLayout/Header/Player/Player.tsx
+++ src/components/layouts/ErdDiagramLayout/Header/Player/Player.tsx
@@ -1,6 +1,7 @@
 import {IUser} from "../../../../../types/data/user";
 import {ActionIcon, Avatar, Tooltip} from "@mantine/core";
 import {useErdDiagramStore} from "@/stores/useErdDiagramStore.ts";
+import {useAuthStore} from "@/stores/useAuthStore.ts";
 
 interface Props {
   player: IUser
@@ -9,8 +10,9 @@ interface Props {
 export default function Player(props: Props) {
   const subscribedTo = useErdDiagramStore(state => state.subscribedTo)
   const setSubscribedTo = useErdDiagramStore(state => state.setSubscribedTo)
+  const authorizedUser = useAuthStore(state => state.getAuthorization())
   const isSubscribedPlayer = subscribedTo?.id === props.player.id
-  const avatarStyle = isSubscribedPlayer? {border: "1px solid var(--mantine-primary-color-filled)"}: {}
+  const avatarStyle = isSubscribedPlayer? {border: "1px solid var(--mantine-primary-color-filled)", zIndex: 1}: {}
   const tooltipLabel = isSubscribedPlayer? `Subscribed to ${props.player.name}` :`Subscribe to ${props.player.name}`
   const onSubscribe = () => {
     setSubscribedTo(props.player)
@@ -18,7 +20,7 @@ export default function Player(props: Props) {
 
   return (
     <Tooltip label={tooltipLabel}>
-      <ActionIcon variant={"transparent"} onClick={onSubscribe}>
+      <ActionIcon disabled={props.player.id === authorizedUser?.id} variant={"transparent"} onClick={onSubscribe}>
         <Avatar style={avatarStyle}>
           {props.player.name[0]}
         </Avatar>
