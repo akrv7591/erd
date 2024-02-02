bafc32f8a67f72f51046691c6071157660b65f61
a7591 <a7591@zeroweb.kr>
2024-02-02 10:22:38 +0900

    removed temporary refreshToken function

--- src/utility/StorageUtils.ts
+++ src/utility/StorageUtils.ts
@@ -1,7 +1,3 @@
-import dayjs from "dayjs";
-import {decodeJwt} from "jose";
-import {refreshToken} from "@/api/erdApi.tsx";
-
 export default class StorageUtils {
   static setAuthorization(authorization: string) {
     localStorage.removeItem("Authorization")
@@ -9,13 +5,6 @@ export default class StorageUtils {
 
     localStorage.setItem("Authorization", authorization)
     sessionStorage.setItem("Authorization", authorization)
-    const decoded = decodeJwt(authorization)
-
-    const expireDate = dayjs.unix(decoded.exp as number).subtract(5, 'seconds')
-    setTimeout(() => {
-      refreshToken()
-    }, expireDate.diff(dayjs(), 'milliseconds'))
-
   }
 
   static getAuthorization() {
