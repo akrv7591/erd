061705b970320ec695380ba3d9da775ef8de8e18
a7591 <a7591@zeroweb.kr>
2024-02-19 13:50:44 +0900

    updated Title style

--- src/screens/Playground/Header/Title/index.tsx
+++ src/screens/Playground/Header/Title/index.tsx
@@ -1,12 +1,14 @@
-import {Title as MantineTitle} from "@mantine/core";
+import {Paper, Title as MantineTitle} from "@mantine/core";
 import {usePlaygroundStore} from "@/stores/usePlaygroundStore.ts";
 
 export default function Title() {
   const name = usePlaygroundStore(state => state.name)
 
   return (
-    <MantineTitle order={3} style={{textTransform: "none", borderRadius: "5px"}}>
-      {name}
-    </MantineTitle>
+    <Paper bg={"var(--mantine-color-dark-6)"} px={"15px"} py={"5px"}>
+      <MantineTitle order={4} fw={"normal"} style={{textTransform: "none", borderRadius: "5px"}}>
+        {name}
+      </MantineTitle>
+    </Paper>
   )
 }
