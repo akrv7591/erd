b4068d067f0d2f1baffa7172851028c2a2c70ee5
a7591 <a7591@zeroweb.kr>
2024-03-15 11:22:14 +0900

    added Show memo controller button

---
+++ src/screens/Playground/Aside/MemoController.tsx
@@ -0,0 +1,23 @@
+import {usePlaygroundStore} from "@/stores/usePlaygroundStore.ts";
+import {ActionIcon, Tooltip} from "@mantine/core";
+import {IconNotes, IconNotesOff} from "@tabler/icons-react";
+
+export default function MemoController() {
+  const showMemos = usePlaygroundStore(state => state.showMemos)
+  const setShowMemos = usePlaygroundStore(state => state.setShowMemos)
+  const toggle = () => setShowMemos(!showMemos)
+  return (
+    <Tooltip label={showMemos? "Hide showMemos": "Show showMemos"} position={"left"}>
+      <ActionIcon
+        mx={"5px"}
+        w={"40px"}
+        mt={"auto"}
+        h={"40px"}
+        variant={"default"}
+        onClick={toggle}
+      >
+        {showMemos? <IconNotesOff/>: <IconNotes />}
+      </ActionIcon>
+    </Tooltip>
+  )
+}
