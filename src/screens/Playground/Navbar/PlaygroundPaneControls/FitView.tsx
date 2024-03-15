db8406a1636a46b46765f3ecb59f025ca411fbd8
a7591 <a7591@zeroweb.kr>
2024-03-15 14:59:10 +0900

    updated FitView icon

--- src/screens/Playground/Navbar/PlaygroundPaneControls/FitView.tsx
+++ src/screens/Playground/Navbar/PlaygroundPaneControls/FitView.tsx
@@ -1,6 +1,6 @@
 import {ActionIcon, Tooltip} from "@mantine/core";
 import {useReactFlow} from "@xyflow/react";
-import {IconMaximize} from "@tabler/icons-react";
+import {IconFocusCentered} from "@tabler/icons-react";
 
 export default function FitView() {
   const reactflow = useReactFlow()
@@ -10,7 +10,7 @@ export default function FitView() {
   return (
     <Tooltip position={"right"} withArrow label={"Fit view"}>
       <ActionIcon h={"40px"} w={"40px"} variant={"default"} onClick={handleOverview}>
-        <IconMaximize />
+        <IconFocusCentered />
       </ActionIcon>
     </Tooltip>
   )
