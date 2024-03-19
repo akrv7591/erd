2b70129938a39ed1897494688d0df77ca68a5934
a7591 <a7591@zeroweb.kr>
2024-03-19 10:35:51 +0900

    added NODE_TYPES enum

--- src/screens/Playground/Main/nodes/index.ts
+++ src/screens/Playground/Main/nodes/index.ts
@@ -2,7 +2,12 @@ import type {NodeTypes} from "@xyflow/react";
 import EntityNode from "./EntityNode";
 import MemoNode from "@/screens/Playground/Main/nodes/MemoNode";
 
+export enum NODE_TYPES {
+  ENTITY = "entity",
+  MEMO = "memo"
+}
+
 export const nodeTypes: NodeTypes = {
-  entityNode: EntityNode,
-  memoNode: MemoNode
+  [NODE_TYPES.ENTITY]: EntityNode,
+  [NODE_TYPES.MEMO]: MemoNode
 }
