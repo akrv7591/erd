a0a1a8596f231ac944ae68067e3686f094896b88
a7591 <a7591@zeroweb.kr>
2024-08-28 13:30:35 +0900

    fixed PROJECT constant

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -2,4 +2,5 @@ export const PROJECT = {
   NAME: "ERDiagramly",
   BASE_API_URL: "https://erdiagramly-api.akrv.dev",
   // BASE_API_URL: "http://127.0.0.1:3001",
+  WEBRTC_SIGNALLING_SERVERS: ["wss://signalling.akrv.dev"]
 }
