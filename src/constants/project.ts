5b6996fd2161d15142eb96ec6c86a858ea672517
a7591 <a7591@zeroweb.kr>
2024-10-16 16:07:02 +0900

    removed unused constants

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -1,6 +1,3 @@
 export const PROJECT = {
   NAME: "ERDiagramly",
-  BASE_API_URL: "https://erdiagramly-api.akrv.dev",
-  // BASE_API_URL: "http://127.0.0.1:3001",
-  WEBRTC_SIGNALLING_SERVERS: ["wss://signalling.akrv.dev"]
 }
