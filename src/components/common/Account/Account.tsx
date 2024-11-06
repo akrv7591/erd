a57cbbf7299db2dfff8310b059865c63adea949d
a7591 <a7591@zeroweb.kr>
2024-11-06 12:03:43 +0900

    - Temporary disabled profile screen

--- src/components/common/Account/Account.tsx
+++ src/components/common/Account/Account.tsx
@@ -1,44 +1,47 @@
-import {ActionIcon, Menu, rem} from "@mantine/core";
-import {IconLogout, IconUserCog} from "@tabler/icons-react";
-import {useNavigate} from "react-router-dom";
-import {useUser} from "@/hooks";
-import {useLogto} from "@logto/react";
-import {memo, useCallback} from "react";
-import {config} from "@/config/config";
-import {ProfilePicture} from "@/components/common/ProfilePicture";
+import { ActionIcon, Menu, rem } from "@mantine/core";
+import { IconLogout, IconUserCog } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "@/hooks";
+import { useLogto } from "@logto/react";
+import { memo, useCallback } from "react";
+import { config } from "@/config/config";
+import { ProfilePicture } from "@/components/common/ProfilePicture";
 
 export const Account = memo(() => {
-  const {signOut} = useLogto()
-  const user = useUser()
-  const navigate = useNavigate()
-  const handleLogout = useCallback(() => signOut(config.client.url), [])
+  const { signOut } = useLogto();
+  const user = useUser();
+  const navigate = useNavigate();
+  const handleLogout = useCallback(() => signOut(config.client.url), []);
 
   return (
     <Menu shadow="md" width={200}>
       <Menu.Target>
         <ActionIcon size={40} variant={"transparent"}>
-          <ProfilePicture src={user.picture} avatarProps={{size: "md"}}/>
+          <ProfilePicture src={user.picture} avatarProps={{ size: "md" }} />
         </ActionIcon>
       </Menu.Target>
 
       <Menu.Dropdown>
         <Menu.Item
-          leftSection={(
-            <IconUserCog style={{width: rem(16), height: rem(16)}}/>
-          )}
+          disabled
+          leftSection={
+            <IconUserCog style={{ width: rem(16), height: rem(16) }} />
+          }
           onClick={() => navigate("/profile/setting")}
         >
           Profile settings
         </Menu.Item>
-        <Menu.Divider/>
+        <Menu.Divider />
         <Menu.Item
           color="red"
-          leftSection={<IconLogout style={{width: rem(16), height: rem(16)}}/>}
+          leftSection={
+            <IconLogout style={{ width: rem(16), height: rem(16) }} />
+          }
           onClick={handleLogout}
         >
           Log out
         </Menu.Item>
       </Menu.Dropdown>
     </Menu>
-  )
-})
+  );
+});
