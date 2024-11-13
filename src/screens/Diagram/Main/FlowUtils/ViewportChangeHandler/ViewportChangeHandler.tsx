97d77536d3a88a0d3da93d9021963a55ccda3c04
a7591 <a7591@zeroweb.kr>
2024-11-13 18:34:47 +0900

    - Rerender User cursor on viewport change

--- src/screens/Diagram/Main/FlowUtils/ViewportChangeHandler/ViewportChangeHandler.tsx
+++ src/screens/Diagram/Main/FlowUtils/ViewportChangeHandler/ViewportChangeHandler.tsx
@@ -1,11 +1,17 @@
-import {memo} from "react";
+import {memo, useCallback} from "react";
+import {useOnViewportChange} from "@xyflow/react";
+import {useDiagramStoreApi} from "@/hooks";
 // import {useOnViewportChange} from "@xyflow/react";
 // import {useAwareness} from "@/contexts/AwarenessContext";
 
 export const ViewportChangeHandler = memo(() => {
-  // const {handleViewportChange} = useAwareness()
-
-  // useOnViewportChange({onChange: handleViewportChange})
+  const storeApi = useDiagramStoreApi()
+  const handleViewportChange = useCallback(() => {
+    storeApi.setState(state => ({
+      clients: [...state.clients]
+    }))
+  }, [])
+  useOnViewportChange({onChange: handleViewportChange})
 
   return null
 })
