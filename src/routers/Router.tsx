928e912f31bddde3e26e7325c4ce3bf99915eed1
a7591 <a7591@zeroweb.kr>
2024-12-03 11:41:28 +0900

    - Add initial load state to Router and remove - @logto/browser dependency.

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,15 +1,21 @@
 import { RouterProvider } from "react-router-dom";
 import { useLogto } from "@logto/react";
 import { routes } from "@/routers/routes";
-import { memo, Suspense, useEffect } from "react";
+import { memo, Suspense, useEffect, useState } from "react";
 import { Axios } from "@/services";
 
 export const Router = memo(() => {
+  const [initialLoad, setInitialLoad] = useState(true);
   const logTo = useLogto();
 
   useEffect(() => {
     Axios.instance.logTo = logTo;
-  }, []);
+    setInitialLoad(false)
+  }, [logTo])
+
+  if (initialLoad) {
+    return null
+  }
 
   return (
     <Suspense>
