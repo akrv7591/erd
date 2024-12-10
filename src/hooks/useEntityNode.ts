b7b4fab890c5bd0cb7bc534da3e96db4a0cf48ff
a7591 <a7591@zeroweb.kr>
2024-12-10 12:26:23 +0900

    - Added useEntityColor hook

--- src/hooks/useEntityNode.ts
+++ src/hooks/useEntityNode.ts
@@ -1,4 +1,4 @@
-import {useCallback, useMemo} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 import {useNodeId, useNodesData, useReactFlow} from "@xyflow/react";
 import type {EntityNode} from "@/types/diagram";
 
@@ -27,4 +27,15 @@ export const useEntityNode = () => {
       onDelete,
     }
   }, [node])
+}
+
+export const useEntityColor = () => {
+  const entityNode = useEntityNode()
+  const [color, setColor] = useState(entityNode.data.color)
+
+  useEffect(() => {
+    setColor(entityNode.data.color)
+  }, [entityNode.data.color])
+
+  return color
 }
