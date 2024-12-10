e9dc4980ed7765bb02639014447ad4479203852e
a7591 <a7591@zeroweb.kr>
2024-12-10 12:29:10 +0900

    - Rendering optimization for EntityNode

--- src/screens/Diagram/Main/NodeTypes/EntityNode/Content/Content.tsx
+++ src/screens/Diagram/Main/NodeTypes/EntityNode/Content/Content.tsx
@@ -1,24 +1,21 @@
-import {memo} from "react";
-import {Card} from "@mantine/core";
-import {CustomTheme} from "@/components/common/CustomTheme";
-import {Header} from "./Header";
-import {Table} from "./Table";
-import {RelationsOverlay} from "./RelationsOverlay";
-import {NameOverlay} from "./NameOverlay";
-import {useEntityNode} from "@/hooks";
+import { memo } from "react";
+import { Card } from "@mantine/core";
+import { Header } from "./Header";
+import { Table } from "./Table";
+import { RelationsOverlay } from "./RelationsOverlay";
+import { NameOverlay } from "./NameOverlay";
+
 import "./style.css"
 
 export const Content = memo(() => {
-  const {id, data} = useEntityNode()
-
   return (
-    <CustomTheme color={data.color} id={id}>
+    <>
       <Card className={"entity-container"} withBorder>
-        <Header/>
-        <Table/>
+        <Header />
+        <Table />
       </Card>
-      <RelationsOverlay/>
+      <RelationsOverlay />
       <NameOverlay />
-    </CustomTheme>
+    </>
   )
 })
--- src/screens/Diagram/Main/NodeTypes/EntityNode/EntityNode.tsx
+++ src/screens/Diagram/Main/NodeTypes/EntityNode/EntityNode.tsx
@@ -1,10 +1,24 @@
-import {FC} from "react";
-import {NodeProps} from "@xyflow/react";
-import {Content} from "./Content";
-import type {EntityNode as EntityNodeType} from "@/types/diagram";
+import { FC, memo } from "react";
+import { NodeProps } from "@xyflow/react";
+import { Content } from "./Content";
+import type { EntityNode as EntityNodeType } from "@/types/diagram";
+import { CustomTheme } from "@/components/common/CustomTheme";
 
-export const EntityNode: FC<NodeProps<EntityNodeType>> = () => {
+interface ThemeProps {
+  color: string
+  id: string
+}
+
+const Theme: FC<ThemeProps> = memo(({id, color}) => {
   return (
-    <Content />
+    <CustomTheme color={color} id={id}>
+      <Content />
+    </CustomTheme>
+  )
+})
+
+export const EntityNode: FC<NodeProps<EntityNodeType>> = ({ data, id }) => {
+  return (
+      <Theme color={data.color} id={id}/>
   )
 }
